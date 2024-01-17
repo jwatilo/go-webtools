fcde4d4e7c1e1d602d318fc4708650da502b89b3
John Watilo <john.watilo@sabre.com>
2024-01-17 11:31:10 -0600

    Added basic functionality.

--- tools.go
+++ tools.go
@@ -38,10 +38,33 @@ func (t *Tools) RandomString(n int) string {
 // UploadedFile is a struct used to save information about an uploaded file
 type UploadedFile struct {
 	NewFileName      string
-	OriginalFIleName string
+	OriginalFileName string
 	FileSize         int64
 }
 
+// UploadOneFile is just a convenience method that calles UploadFiles, but expects only one file to
+// be in the upload.
+func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool) (*UploadedFile, error) {
+
+	renameFile := true
+	if len(rename) > 0 {
+
+		renameFile = rename[0]
+	}
+
+	files, err := t.UploadFiles(r, uploadDir, renameFile)
+	if err != nil {
+
+		return nil, err
+	}
+
+	return files[0], nil
+}
+
+// UploadFiles uploads one or more files to a specified directory, and gives the files a random name.
+// It returns a slice containing the newly named files, the original file names, the size of the files,
+// and potentially an error. If the optional last parameter is set to true, then we will not rename
+// the files, but will use the original file names.
 func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
 
 	renameFile := true
@@ -120,6 +143,7 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 
 					uploadedFile.NewFileName = hdr.Filename
 				}
+				uploadedFile.OriginalFileName = hdr.Filename
 
 				var outfile *os.File
 				defer outfile.Close()
